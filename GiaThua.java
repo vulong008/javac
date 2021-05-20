49c7a375ac3eee203a91e3ed210a5ae83321eac3
vulong008 <84512747+vulong008@users.noreply.github.com>
2021-05-20 21:08:19 +0700

    Add files via upload

---
+++ GiaThua.java
@@ -0,0 +1,24 @@
+package giathua;
+
+import java.util.Scanner;
+
+public class GiaThua {
+
+   public static long giaiThua(int n)
+   {
+       if(n==0 || n==1)
+           return 1;
+       else
+           return n*giaiThua(n-2);
+   }
+   
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+        System.out.println("nhap n: ");
+        int n= sc.nextInt();
+        //long sum = 1;
+        System.out.println("giai thua la: " + giaiThua(n));
+    }
+    
+}
+
