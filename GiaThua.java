295882e79006f5886a4a333ba7c4336024cc5d54
vulong008 <84512747+vulong008@users.noreply.github.com>
2021-05-20 21:21:35 +0700

    Update GiaThua.java

--- GiaThua.java
+++ GiaThua.java
@@ -14,9 +14,8 @@ public class GiaThua {
    
     public static void main(String[] args) {
        Scanner sc =new Scanner(System.in);
-        System.out.println("nhap n: ");
+       System.out.println("nhap n: ");
         int n= sc.nextInt();
-        //long sum = 1;
         System.out.println("giai thua la: " + giaiThua(n));
     }
     
